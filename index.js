8f89c521a6512c72ef5528749f3526509d6d7730
IvanCIvanov <vivanov@webcluster.com>
2023-05-27 21:59:25 -0400

    testpage,aboutme,scripts

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("I like pizza.");
+console.log("Hello World");
+
+alert("Warning")
+
+// this is a comment
+
+let username;
+document.getElementById("myButton").onclick = function () {
+
+    username = document.getElementById("myText").value;
+
+    console.log(username);
+}
